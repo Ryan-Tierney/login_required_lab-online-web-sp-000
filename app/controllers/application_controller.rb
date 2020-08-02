ff7e905a09b4c25f877df3d032c3b977b9ddab46
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-02 21:11:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello 
-    redirect_to controller: 'sessions' action: 'new' unless session[:name]
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
   end 
   
   def current_user 
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
   private 
   
   def require_logged_in?
-    redirect_to controller: 'sessions' action: 'new' unless current_user
+    redirect_to controller: 'sessions', action: 'new' unless current_user
   end 
 end

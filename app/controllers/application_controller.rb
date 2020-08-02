dc83cbb9599b70dc367b4af741230ce7f7bdc8e9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-02 21:11:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello 
+    redirect_to controller: 'sessions' action: 'new' unless session[:name]
   end 
   
   def current_user 
